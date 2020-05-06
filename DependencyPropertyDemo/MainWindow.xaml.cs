75a4a3c514e29cc65f433462bac739664b9eab15
KulaGGin <sergeant.coolagin@gmail.com>
2020-05-06 15:51:26 +0300

    Add project files.

---
+++ DependencyPropertyDemo/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+
+namespace WpfApplication1 {
+    /// <summary>  
+    /// Interaction logic for DependencyPropertyDemo.xaml  
+    /// </summary>  
+    public partial class DependencyPropertyDemo : Window {
+        public DependencyPropertyDemo() {
+            InitializeComponent();
+        }
+        private void MyButton_Click(object sender, RoutedEventArgs e) {
+            CarDependencyClass dpSample = TryFindResource("carDependencyClass") as CarDependencyClass;
+            MessageBox.Show(dpSample.MyCar);
+        }
+    }
+    public class CarDependencyClass : DependencyObject {
+        //Register Dependency Property  
+        public static readonly DependencyProperty CarDependencyProperty = DependencyProperty.Register("MyProperty", typeof(string), typeof(CarDependencyClass));
+        public string MyCar {
+            get {
+                return (string)GetValue(CarDependencyProperty);
+            }
+            set {
+                SetValue(CarDependencyProperty, value);
+            }
+        }
+    }
+}
---
+++ DependencyPropertyTutorial/CustomButtonControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+
+namespace DependencyPropertyTutorial {
+    /// <summary>  
+    /// Interaction logic for CustomButtonControl.xaml  
+    /// </summary>  
+    public partial class CustomButtonControl : UserControl {
+        public CustomButtonControl() {
+            InitializeComponent();
+        }
+        public static readonly DependencyProperty btnDependencyProperty = DependencyProperty.Register("SetBackground", typeof(SolidColorBrush), typeof(CustomButtonControl), new PropertyMetadata(new SolidColorBrush(Colors.HotPink), new PropertyChangedCallback(OnSetColorChanged)));
+        public SolidColorBrush SetBackground {
+            set {
+                SetValue(btnDependencyProperty, value);
+            }
+            get {
+                return (SolidColorBrush)GetValue(btnDependencyProperty);
+            }
+        }
+        private void btnCustom_Click(object sender, RoutedEventArgs e) {
+            this.SetBackground = new SolidColorBrush(Colors.IndianRed);
+        }
+        private static void OnSetColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+            CustomButtonControl mycontrol = d as CustomButtonControl;
+            mycontrol.callmyInstanceMethod(e);
+        }
+        private void callmyInstanceMethod(DependencyPropertyChangedEventArgs e) {
+            btnCustom.Background = (SolidColorBrush)e.NewValue;
+        }
+    }
+}
